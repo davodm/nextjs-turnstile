555fe9d0e538e0994933648f0a67ee4d7db9f49b
davodm <irdavidnet@gmail.com>
2024-09-23 15:59:21 +0100

    fix: explicit way updated in the way of calling callbacks

--- src/components/TurnstileExplicit.js
+++ src/components/TurnstileExplicit.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect } from "react";
+import loadTurnstileScript from "../utils/loadScript";
 
 let explicitFields = []; // Array to track the explicit CAPTCHA fields
 
 /**
  * TurnstileExplicit component registers and initializes the Cloudflare Turnstile CAPTCHA in explicit mode.
- * 
+ *
  * This component registers CAPTCHA instances, which must be manually initialized using the provided
  * response field name as the ID for the corresponding `<div>` element. The initialization script will
  * render the CAPTCHA based on the specified theme and size.
@@ -28,18 +29,24 @@ const TurnstileExplicit = ({
   onSuccess = () => {},
 }) => {
   const siteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY;
-  const JSUrl = "https://challenges.cloudflare.com/turnstile/v0/api.js?onload=turnstileReady";
 
   useEffect(() => {
-    // Register the field for later initialization
-    // Check if the field is already registered
-    if (explicitFields.some((field) => field.fieldName === responseFieldName)) {
-      return;
+    // Prevent Duplicate `responseFieldName`
+    const isDuplicate = explicitFields.some(
+      (field) => field.fieldName === responseFieldName
+    );
+    if (isDuplicate) {
+      throw new Error(
+        `Duplicate responseFieldName "${responseFieldName}" detected. Each TurnstileExplicit component must have a unique responseFieldName.`
+      );
     }
+
+    // Register the field for later initialization
     explicitFields.push({
       fieldName: responseFieldName,
       size,
       theme,
+      callbacks: { onExpire, onError, onSuccess },
     });
 
     return () => {
@@ -48,34 +55,72 @@ const TurnstileExplicit = ({
         (field) => field.fieldName !== responseFieldName
       );
     };
-  }, [responseFieldName, size, theme]);
+  }, [responseFieldName, size, theme, onSuccess, onError, onExpire]);
 
   useEffect(() => {
-    // Ensure the Turnstile script is loaded only once
-    if (!document.querySelector('script[src="' + JSUrl + '"]')) {
-      const script = document.createElement("script");
-      script.src = JSUrl;
-      script.async = true;
-      document.body.appendChild(script);
+    // Load the Turnstile script in explicit mode
+    loadTurnstileScript(true)
+      .then(() => {
+        // Define the global callback function turnstileReady
+        if (
+          typeof window.turnstile !== "undefined" &&
+          window.turnstile &&
+          window.turnstileReady
+        ) {
+          // Define Function
+          window.turnstileReady = function () {
+            explicitFields.forEach(function (field) {
+              // Define unique callback function names
+              const CALLBACK_PREFIX = `turnstileCallbacks_${field.fieldName}`;
+              const CALLBACK_NAMES = {
+                verify: `${CALLBACK_PREFIX}_verify`,
+                error: `${CALLBACK_PREFIX}_error`,
+                expire: `${CALLBACK_PREFIX}_expire`,
+                timeout: `${CALLBACK_PREFIX}_timeout`,
+              };
+              // Assign global callback functions
+              window[CALLBACK_NAMES.verify] = (token) => {
+                field.callbacks.onSuccess(token);
+              };
 
-      script.onload = () => {
-        window.turnstileReady = function () {
-          explicitFields.forEach(function (field) {
-            window.turnstile.render(`#${field.fieldName}`, {
-              sitekey: siteKey,
-              "response-field-name": field.fieldName,
-              size: field.size,
-              theme: field.theme,
-              "error-callback": onError || (() => {}),
-              "expired-callback": onExpire || (() => {}),
-              callback: onSuccess || (() => {}),
-              "timeout-callback": () => window.turnstile.reset(`#${field.fieldName}`),
+              window[CALLBACK_NAMES.error] = () => {
+                field.callbacks.onError();
+              };
+
+              window[CALLBACK_NAMES.expire] = () => {
+                field.callbacks.onExpire();
+              };
+
+              window[CALLBACK_NAMES.timeout] = () => {
+                if (
+                  window.turnstile &&
+                  typeof window.turnstile.reset === "function"
+                ) {
+                  window.turnstile.reset(`#${field.fieldName}`);
+                }
+              };
+              // Render the CAPTCHA widget
+              window.turnstile.render(`#${field.fieldName}`, {
+                sitekey: siteKey,
+                "response-field-name": field.fieldName,
+                size: field.size,
+                theme: field.theme,
+                "error-callback": CALLBACK_NAMES.error,
+                "expired-callback": CALLBACK_NAMES.expire,
+                callback: CALLBACK_NAMES.verify,
+                "timeout-callback": CALLBACK_NAMES.timeout,
+              });
             });
-          });
-        };
-      };
-    }
-  }, []);
+          };
+        } else if (window?.turnstileReady) {
+          // If the script is already loaded, initialize the new field
+          window.turnstileReady();
+        }
+      })
+      .catch((error) => {
+        throw new Error(`Failed to load Turnstile script: ${error}`);
+      });
+  }, [siteKey]);
 
   return null; // No need to render anything, just register the field
 };
